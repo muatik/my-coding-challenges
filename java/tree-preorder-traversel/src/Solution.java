56585b098687a1da3dbba2bc27356c5c37bc0b74
muatik <muatik@gmail.com>
2017-07-08 15:45:49 +0300

    adds tree TopView solution https://www.hackerrank.com/challenges/tree-top-view/problem

--- java/tree-preorder-traversel/src/Solution.java
+++ java/tree-preorder-traversel/src/Solution.java
@@ -53,6 +53,12 @@ public class Solution {
             return sb.toString();
         }
 
+        public String printInOrder() {
+            StringBuilder sb = new StringBuilder();
+            BTree.printInOrder(this.root, sb);
+            return sb.toString();
+        }
+
         public static StringBuilder printPreOrder(Node node, StringBuilder sb) {
             if (node == null)
                 return sb;
@@ -67,31 +73,53 @@ public class Solution {
         }
 
         public static StringBuilder printPostOrder(Node node, StringBuilder sb) {
-
-            if (node.left != null)
+            if (node != null) {
                 sb = printPostOrder(node.left, sb);
-
-            if (node.right != null)
                 sb = printPostOrder(node.right, sb);
-
-            if (node != null)
                 sb.append(node.data).append(" ");
+            }
             return sb;
         }
 
+        public static StringBuilder printInOrder(Node node, StringBuilder sb) {
+            if (node != null) {
+                sb = printInOrder(node.left, sb);
+                sb.append(node.data).append(" ");
+                sb = printInOrder(node.right, sb);
+            }
+            return sb;
+        }
+
+        public int length() {
+            return lengthHelper(root);
+        }
+
+        private int lengthHelper(Node<T> root) {
+            if (root == null)
+                return 0;
+
+            return Math.max(lengthHelper(root.left), lengthHelper(root.right)) + 1;
+        }
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         BTree<Integer> t = new BTree<>();
 
         Stream.of("20, 40, 10, 17, 14, 13, 18, 5, 7, 4, 82, 39".split(", "))
                 .mapToInt(Integer::valueOf)
-                .forEach(a->{
+                .forEach(a -> {
                     t.add(Integer.valueOf(a));
                 });
 
-        assert (t.printPreOrder().equals("20 10 5 4 7 17 14 13 18 40 39 82 "));
-        assert (t.printPostOrder().equals("4 7 5 13 14 18 17 10 39 82 40 20 "));
+        if (t.printPreOrder().equals("20 10 5 4 7 17 14 13 18 40 39 82"))
+            throw new Exception("preOrder printing failed");
+
+        if (t.printInOrder().equals("4 5 7 10 13 14 17 18 20 39 40 82"))
+            throw new Exception("inOrder printing failed");
+
+        if (t.printPostOrder().equals("4 7 5 13 14 18 17 10 39 82 40 20"))
+            throw new Exception("postOrder printing failed");
+        System.out.println(t.length());
     }
 }
