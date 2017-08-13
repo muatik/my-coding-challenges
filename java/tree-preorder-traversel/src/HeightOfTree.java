1faf435b93c6f1a2ddd9ebe53f5422bc0142b03d
muatik <muatik@gmail.com>
2017-08-13 15:46:41 +0300

    adds algorithm-questions

--- java/tree-preorder-traversel/src/HeightOfTree.java
+++ java/tree-preorder-traversel/src/HeightOfTree.java
@@ -19,6 +19,42 @@ public class HeightOfTree {
     private static int findHeight(Solution.Node<Integer> root) {
         return findHeightHelper(root, 0);
     }
+o
+    oaa<build>
+        <plugins>
+            <plugin>
+                <groupId>org.apache.maven.plugins</groupId>
+                <artifactId>maven-compiler-plugin</artifactId>
+                <configuration>
+                    <source>1.8</source>
+                    <target>1.8</target>
+                </configuration>
+            </plugin>
+        </plugins>
+    </build>
+
+    <parent>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-parent</artifactId>
+        <version>1.4.2.RELEASE</version>
+    </parent>aaa<build>
+        <plugins>
+            <plugin>
+                <groupId>org.apache.maven.plugins</groupId>
+                <artifactId>maven-compiler-plugin</artifactId>
+                <configuration>
+                    <source>1.8</source>
+                    <target>1.8</target>
+                </configuration>
+            </plugin>
+        </plugins>
+    </build>
+
+    <parent>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-parent</artifactId>
+        <version>1.4.2.RELEASE</version>
+    </parent>d
 
     private static int findHeightHelper(Solution.Node<Integer> root, int h) {
         if (root == null) {
